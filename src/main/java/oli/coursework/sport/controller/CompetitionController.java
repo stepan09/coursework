50563d88793a6a3765f287cc07939f667f17dd5a
Stepan Oliinyk <stepanoliinyk09@gmail.com>
2018-04-07 09:01:21 +0300

    added CompetitionController's realization

--- src/main/java/oli/coursework/sport/controller/CompetitionController.java
+++ src/main/java/oli/coursework/sport/controller/CompetitionController.java
@@ -4,15 +4,17 @@
 
 package oli.coursework.sport.controller;
 
+import oli.coursework.sport.exception.ResourceNotFoundException;
 import oli.coursework.sport.model.Competition;
 import oli.coursework.sport.repository.CompetitionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api")
 public class CompetitionController {
@@ -23,5 +25,41 @@ public class CompetitionController {
     @GetMapping("/competitions")
     public List<Competition> getAllCompetitions(){
         return competitionRepository.findAll();
+    }
+
+    @PostMapping("/competitions")
+    public Competition createCompetition(@Valid @RequestBody Competition competition) {
+        return competitionRepository.save(competition);
+    }
+
+    @GetMapping("/competitions/{id}")
+    public Competition getCompetitionById(@PathVariable(value = "id") Long competitionId) {
+        return competitionRepository.findById(competitionId)
+                .orElseThrow(() -> new ResourceNotFoundException("Gym", "id", competitionId));
+    }
+
+    @PutMapping("/competitions/{id}")
+    public Competition updateCompetition(@PathVariable(value = "id") Long competitionId,
+                                         @Valid @RequestBody Competition competitionDetails) {
+        Competition competition = competitionRepository.findById(competitionId)
+                .orElseThrow(() -> new ResourceNotFoundException("Gym", "id", competitionId));
+
+        competition.setName(competitionDetails.getName());
+        competition.setStartDate(competitionDetails.getStartDate());
+        competition.setFinishDate(competitionDetails.getFinishDate());
+
+        Competition updateCompetition = competitionRepository.save(competition);
+
+        return updateCompetition;
+    }
+
+    @DeleteMapping("/competitions/{id}")
+    public ResponseEntity<?> deleteCompetition(@PathVariable(name = "id") Long competitionId) {
+        Competition competition = competitionRepository.findById(competitionId)
+                .orElseThrow(() -> new ResourceNotFoundException("Gym", "id", competitionId));
+
+        competitionRepository.delete(competition);
+
+        return ResponseEntity.ok().build();
     }
 }
