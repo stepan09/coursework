9e5a1f9eae7d24c8930b5769cdbdbcb3e37f1744
Stepan Oliinyk <stepanoliinyk09@gmail.com>
2018-04-07 08:37:56 +0300

    added GymController's realization

--- src/main/java/oli/coursework/sport/controller/GymController.java
+++ src/main/java/oli/coursework/sport/controller/GymController.java
@@ -4,15 +4,17 @@
 
 package oli.coursework.sport.controller;
 
+import oli.coursework.sport.exception.ResourceNotFoundException;
 import oli.coursework.sport.model.Gym;
 import oli.coursework.sport.repository.GymRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api")
 public class GymController {
@@ -24,4 +26,41 @@ public class GymController {
     public List<Gym> getAllGyms(){
         return gymRepository.findAll();
     }
+
+    @PostMapping("/gyms")
+    public Gym createGym(@Valid @RequestBody Gym gym) {
+        return gymRepository.save(gym);
+    }
+
+    @GetMapping("/gyms/{id}")
+    public Gym getGymById(@PathVariable(value = "id") Long gymId) {
+        return gymRepository.findById(gymId)
+                .orElseThrow(() -> new ResourceNotFoundException("Gym", "id", gymId));
+    }
+
+    @PutMapping("/gyms/{id}")
+    public Gym updateGym(@PathVariable(value = "id") Long gymId,
+                         @Valid @RequestBody Gym gymDetails) {
+        Gym gym = gymRepository.findById(gymId)
+                .orElseThrow(() -> new ResourceNotFoundException("Gym", "id", gymId));
+
+        gym.setName(gymDetails.getName());
+        gym.setAddress(gymDetails.getAddress());
+        gym.setFoundationDate(gymDetails.getFoundationDate());
+
+        Gym updateGym = gymRepository.save(gym);
+
+        return updateGym;
+    }
+
+    @DeleteMapping("/gyms/{id}")
+    public ResponseEntity<?> deleteGym(@PathVariable(value = "id") Long gymId) {
+        Gym gym = gymRepository.findById(gymId)
+                .orElseThrow(() -> new ResourceNotFoundException("Gym", "id", gymId));
+
+        gymRepository.delete(gym);
+
+        return ResponseEntity.ok().build();
+    }
+
 }
