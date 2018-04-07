eef196db0205482c1c7b8d1fcf7abd72f949ec24
Stepan Oliinyk <stepanoliinyk09@gmail.com>
2018-04-07 08:49:32 +0300

    added CourtController's realization

--- src/main/java/oli/coursework/sport/controller/CourtController.java
+++ src/main/java/oli/coursework/sport/controller/CourtController.java
@@ -4,15 +4,17 @@
 
 package oli.coursework.sport.controller;
 
+import oli.coursework.sport.exception.ResourceNotFoundException;
 import oli.coursework.sport.model.Court;
 import oli.coursework.sport.repository.CourtRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api")
 public class CourtController {
@@ -23,5 +25,42 @@ public class CourtController {
     @GetMapping("/courts")
     public List<Court> getAllCourts(){
         return courtRepository.findAll();
+    }
+
+    @PostMapping("/courts")
+    public Court createCourt(@Valid @RequestBody Court court) {
+        return courtRepository.save(court);
+    }
+
+    @GetMapping("/courts/{id}")
+    public Court getCourtById(@PathVariable(value = "id") Long courtId) {
+        return courtRepository.findById(courtId)
+                .orElseThrow(() -> new ResourceNotFoundException("Gym", "id", courtId));
+    }
+
+    @PutMapping("/courts/{id}")
+    public Court updateCourte(@PathVariable(value = "id") Long courtId,
+                              @Valid @RequestBody Court courtDetails) {
+        Court court = courtRepository.findById(courtId)
+                .orElseThrow(() -> new ResourceNotFoundException("Gym", "id", courtId));
+
+        court.setName(courtDetails.getName());
+        court.setAddress(courtDetails.getAddress());
+        court.setCoverType(courtDetails.getCoverType());
+        court.setFoundationDate(courtDetails.getFoundationDate());
+
+        Court updateCourt = courtRepository.save(court);
+
+        return updateCourt;
+    }
+
+    @DeleteMapping("/courts/{id}")
+    public ResponseEntity<?> deleteCourt(@PathVariable(value = "id") Long courtId) {
+        Court court = courtRepository.findById(courtId)
+                .orElseThrow(() -> new ResourceNotFoundException("Gym", "id", courtId));
+
+        courtRepository.delete(court);
+
+        return ResponseEntity.ok().build();
     }
 }
